547567287b71a807a0ec1d44bbaeec132f30785f
gutelfuldead <gutel@hcs.ufl.edu>
2017-06-08 15:27:54 -0700

    Updated fifo_controller.vhd

--- axi_bram_fifo_controller_1.0/hdl/FIFO_Controller.vhd
+++ axi_bram_fifo_controller_1.0/hdl/FIFO_Controller.vhd
@@ -4,7 +4,7 @@
 -- Create Date: 05/17/2017 09:20:37 AM
 -- Design Name: 
 -- Module Name: FIFO_Controller - Behavioral
--- Target Devices: Zynq7020
+-- Target Devices:  Zynq7020
 -- Tool Versions: Vivado 2015.4
 -- Description:   Controller interface for BRAM block_memory_generator core
 -- 
@@ -22,16 +22,25 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity FIFO_Controller is
     generic (
+           READ_SRC         : std_logic := '1';
            BRAM_ADDR_WIDTH  : integer := 10;
            BRAM_DATA_WIDTH  : integer := 32 );
     Port ( 
-           -- BRAM Control Logic
+           -- BRAM write port lines
            addra : out STD_LOGIC_VECTOR (BRAM_ADDR_WIDTH-1 downto 0);
-           dina : out STD_LOGIC_VECTOR (BRAM_DATA_WIDTH-1 downto 0);
-           douta : in STD_LOGIC_VECTOR (BRAM_DATA_WIDTH-1 downto 0);
-           ena : out STD_LOGIC; -- core general enable
-           wea : out STD_LOGIC; -- core write enable
-           clka : out std_logic;
+           dina  : out STD_LOGIC_VECTOR (BRAM_DATA_WIDTH-1 downto 0);
+           ena   : out STD_LOGIC;
+           wea   : out STD_LOGIC;
+           clka  : out std_logic;
+           rsta  : out std_logic;
+       
+           -- BRAM read port lines
+           addrb : out STD_LOGIC_VECTOR (BRAM_ADDR_WIDTH-1 downto 0);
+           doutb : in STD_LOGIC_VECTOR (BRAM_DATA_WIDTH-1 downto 0);
+           enb   : out STD_LOGIC;
+           clkb  : out std_logic;
+           rstb  : out std_logic;
+           
            -- Core logic
            clk        : in std_logic;
            clkEn      : in std_logic;
@@ -68,14 +77,23 @@ architecture Behavioral of FIFO_Controller is
     constant RDWR_READ  : std_logic := '1';
     constant RDWR_WRITE : std_logic := '0';
     signal full, empty : std_logic := '0';
+    signal rdwr_hist : std_logic := '0';
+    constant PS_READ : std_logic := '1';
+    constant PL_READ : std_logic := '0';
+    
+    type state is (IDLE, READ, READ_PREP, WRITE, WRITE_PREP);
+    signal fsm_state : state := IDLE;
     
 begin
 
     -- instantiate clock at top level with BUFR; leave this port open in instantiation
     clka <= clk;
+    clkb <= clk;
+    ena  <= '1';
+    enb  <= '1';    
+    
     bram_full <= full;
     bram_empty <= empty;
---    bram_occupancy <= std_logic_vector(occupancy);
     
     fifo_addr_gen : addr_gen
     generic map ( BRAM_ADDR_WIDTH => BRAM_ADDR_WIDTH )
@@ -90,97 +108,62 @@ begin
         fifo_full => full,
         fifo_occupancy => bram_occupancy
     );
-            
-    bram_rw_ops : process(clk)
+    
+    bram_rw_states : process(clk)
     begin
     if(rising_edge(clk)) then
-        if(reset = '1') then
+        if(reset='1') then
             dout_valid <= '0';
+            fsm_state <= IDLE;
+            rsta <= '1';
+            rstb <= '1';
         elsif(clkEn = '1') then
-            ena <= '1';
+            rsta <= '0';
+            rstb <= '0';
+       
+            case (fsm_state) is
             
-            if(write_en = '1' and read_en = '0' and full = '0') then
-                    dina <= din;
-                    addra <= wr_addr; 
-                    wea <= '1';
-                    dout_valid <= '0';
-                    addr_en <= '1';
-                    rdwr <= RDWR_WRITE;
-                    
-            elsif(read_en = '1' and write_en = '0' and empty = '0') then
-                    addra <= rd_addr; 
-                    wea <= '0';
-                    dout <= douta;
-                    dout_valid <= '1';
-                    addr_en <= '1';
-                    rdwr <= RDWR_READ;
-            else
-                addr_en <= '0';
+            when IDLE =>
                 wea <= '0';
-            end if;
-        else
-            ena <= '0';
+                addra <= wr_addr;
+                addrb <= rd_addr;
+                if(READ_SRC = PL_READ) then
+                    dout_valid <= '0';
+                end if;
+                addr_en <= '0';
+                if(write_en = '1' and read_en = '0' and full = '0') then
+                    fsm_state <= WRITE_PREP;
+                elsif(write_en = '0' and read_en = '1' and empty = '0') then
+                    fsm_state <= READ_PREP;
+                end if;
+                
+            when WRITE_PREP =>
+                fsm_state <= WRITE;
+                dina <= din;
+                
+            when WRITE =>
+                wea <= '1';
+                addr_en <= '1';
+                rdwr <= RDWR_WRITE;
+                fsm_state <= IDLE;
+                
+            when READ_PREP =>
+                dout_valid <= '0';
+                fsm_state <= READ;
+                
+            when READ =>
+                dout <= doutb;
+                dout_valid <= '1';
+                addr_en <= '1';
+                rdwr <= RDWR_READ;
+                fsm_state <= IDLE;
+                
+            when others =>
+                fsm_state <= IDLE;
+                
+            end case;
         end if;
     end if;
-    end process bram_rw_ops;
-    
---    pointer_arithmetic : process(clk)
---    begin
---    if(rising_edge(clk)) then
---        if(reset = '1') then
---            rd_addr <= (others => '0');
---            wr_addr <= (others => '0');
---            occupancy <= (others => '0');
---        elsif(clkEn = '1') then
---            if(write_en = '0' and read_en = '1') then
---                rd_addr <= rd_addr + 1;
---                if(occupancy > ZERO_OCCUPANCY) then
---                    occupancy <= occupancy - 1;
---                end if;
---            elsif(write_en = '1' and read_en = '0') then
---                wr_addr <= wr_addr + 1;
---                if(occupancy < MAX_OCCUPANCY) then
---                    occupancy <= occupancy + 1;
---                end if;
---            end if;
---            if(occupancy = ZERO_OCCUPANCY) then
---                bram_empty <= '1';
---            elsif(occupancy = MAX_OCCUPANCY) then
---                bram_full <= '1';
---            else   
---                bram_full <= '0';
---                bram_empty <= '0';
---            end if;
---        end if;
---    end if;
---    end process pointer_arithmetic;
-        
---    bram_rw_ops : process(clk)
---    begin
---    if(rising_edge(clk)) then
---        if(reset = '1') then
---            dout_valid <= '0';
---        elsif(clkEn = '1') then
---            ena <= '1';
---            if(write_en = '1' and read_en = '0') then
---                    dina <= din;
---                    addra <= std_logic_vector(wr_addr); 
---                    wea <= '1';
---                    dout_valid <= '0';
---                    bram_full <= '0';
---            elsif(read_en = '1' and write_en = '0') then
---                    addra <= std_logic_vector(rd_addr); 
---                    wea <= '0';
---                    dout <= douta;
---                    dout_valid <= '1';
---                    bram_empty <= '0';
---            else
---                wea <= '0';
---            end if;
---        else
---            ena <= '0';
---        end if;
---    end if;
---    end process bram_rw_ops;
-
+    end process bram_rw_states;
+                
 end Behavioral;
